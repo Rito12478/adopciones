4329642b728aa2cb457314bc22b36964b54fe00c
Rito12478 <50603192+Rito12478@users.noreply.github.com>
2019-07-03 21:07:58 -0500

    Add files via upload

---
+++ Adopciones/Datos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adopciones
+{
+    class Datos
+    {
+        private static Datos instancia;
+        public List<Perro> ListaPerros = new List<Perro>();
+
+        private Datos()
+        {
+        }
+
+        public static Datos GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Datos();
+            }
+
+            return instancia;
+        }
+    }
+}
